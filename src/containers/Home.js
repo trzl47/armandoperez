f24d9f5a7cbcc2a01015d58623a8094992b71d9b
trzl47 <brownty47@gmail.com>
2017-04-02 12:07:44 -0500

    Updated for ES2015
    - made into container component

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,8 +1,10 @@
-var React = require('react');
-import '../../../../../styles/bootstrap-3.3.6-dist/css/bootstrap.min.css';
-import './armandoPerez.css';
+// Libs
+import React, { Component } from 'react';
+// Components
+import Header from './Header.js';
+import HomeSupporting from './HomeSupporting.js';
 
-var ArmandoPerez = React.createClass({
+export default class Home extends Component {
 	// var main = function() {
 	// 	$(css.nav).click(function() {
 	//     var category = $(this).attr('class');
@@ -41,70 +43,12 @@ var ArmandoPerez = React.createClass({
 	//     }
 	//   });
 	// };
-	render: function() {
-	return(
-		<div className={["armandoperez"].join(' ')}>
-			<div className={["header"].join(' ')}>
-			  	<div className={["container"].join(' ')}>
-						<div className={["row"].join(' ')}>
-					  		<div className="col-md-4">
-								<h1>Armando P&eacute;rez</h1>
-					  		</div>
-
-					  		<div className="pull-right">
-									<ul className={["nav"].join(' ')}>
-									  	<li className={["nav-all","active"].join(' ')}><a href="#">All</a></li>
-									  	<li className={["nav-consumer"].join(' ')}><a href="#">Consumer</a></li>
-									  	<li className={["navMobile"].join(' ')}><a href="#">Mobile</a></li>
-									  	<li className={["navCommerce"].join(' ')}><a href="#" >Commerce</a></li>
-									  	<li className={["navEnterprise"].join(' ')}><a href="#">Enterprise</a></li>
-									</ul>
-					  		</div>
-						</div>
-			  	</div>
+	render() {
+		return(
+			<div className='armandoperez'>
+				<Header />
+				<HomeSupporting />
 			</div>
-
-			<div className="main">
-			  	<div className={["container"].join(' ')}>
-						<div className={["row"].join(' ')}>
-					  		<div className="col-md-4">
-									<a className={["consumer","thumbnail"].join(' ')}><img alt='' role='presentation' src={"https://s3.amazonaws.com/codecademy-content/projects/2/armando-perez/p1.jpg"} /></a>
-					  		</div>
-					  		<div className="col-md-4">
-									<a className={["enterprise","thumbnail"].join(' ')}><img alt='' role='presentation' src={"https://s3.amazonaws.com/codecademy-content/projects/2/armando-perez/p2.jpg"} /></a>
-					  		</div>
-						  	<div className="col-md-4">
-									<a className={["consumer","thumbnail"].join(' ')}><img alt='' role='presentation' src={"https://s3.amazonaws.com/codecademy-content/projects/2/armando-perez/p3.jpg"} /></a>
-						  	</div>
-						</div>
-
-						<div className={["row"].join(' ')}>
-						  	<div className="col-md-4">
-									<a className={["commerce","thumbnail"].join(' ')}><img alt='' role='presentation' src={"https://s3.amazonaws.com/codecademy-content/projects/2/armando-perez/p4.jpg"} /></a>
-						  	</div>
-						  	<div className="col-md-4">
-									<a className={["enterprise","thumbnail"].join(' ')}><img alt='' role='presentation' src={"https://s3.amazonaws.com/codecademy-content/projects/2/armando-perez/p5.jpg"} /></a>
-						  	</div>
-						 	<div className="col-md-4">
-								<a className={["mobile","thumbnail"].join(' ')}><img alt='' role='presentation' src={"https://s3.amazonaws.com/codecademy-content/projects/2/armando-perez/p6.jpg"} /></a>
-						  	</div>
-						</div>
-
-						<div className={["row"].join(' ')}>
-						  	<div className="col-md-4">
-									<a className={["mobile","thumbnail"].join(' ')}><img alt='' role='presentation' src={"https://s3.amazonaws.com/codecademy-content/projects/2/armando-perez/p7.jpg"} /></a>
-						  	</div>
-						  		<div className="col-md-4">
-										<a className={["commerce","thumbnail"].join(' ')}><img alt='' role='presentation' src={"https://s3.amazonaws.com/codecademy-content/projects/2/armando-perez/p8.jpg"} /></a>
-						  	</div>
-						  	<div className="col-md-4">
-									<a className={["mobile","thumbnail"].join(' ')}><img alt='' role='presentation' src={"https://s3.amazonaws.com/codecademy-content/projects/2/armando-perez/p9.jpg"} /></a>
-						  	</div>
-						</div>
-			  	</div>
-			</div>
-		</div>
-	);
+		);
+	}
 }
-});
-module.exports = ArmandoPerez;
