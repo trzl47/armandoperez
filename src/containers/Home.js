9357a98665b4b086e1d7ced390595f742e82a835
trzl47 <brownty47@gmail.com>
2017-04-02 18:37:06 -0500

    adding jquery for interaction
    planning to rewrite in vanilla js

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -3,46 +3,35 @@ import React, { Component } from 'react';
 // Components
 import Header from './Header.js';
 import HomeSupporting from './HomeSupporting.js';
+// var main = function() {
+// 	$('.nav li').click(function() {
+//     var category = $(this).attr('class');
+//     $('.nav li').removeClass('active');
+//     $(this).addClass('active');
+//
+//     if(category === 'nav-consumer') {
+//       $('.thumbnail').removeClass('selected');
+//       $('.consumer').addClass('selected');
+//     }
+//     else if(category === 'nav-mobile') {
+//       $('.thumbnail').removeClass('selected');
+//       $('.mobile').addClass('selected');
+//     }
+//     else if(category ==='nav-commerce') {
+//       $('.thumbnail').removeClass('selected');
+//       $('.commerce').addClass('selected');
+//     }
+//     else if(category ==='nav-enterprise') {
+//       $('.thumbnail').removeClass('selected');
+//       $('.enterprise').addClass('selected');
+//     }
+//     else if(category ==='nav-all') {
+//       $('.thumbnail').removeClass('selected');
+//     }
+//   });
+// };
 
 export default class Home extends Component {
-	// var main = function() {
-	// 	$(css.nav).click(function() {
-	//     var category = $(this).attr('class');
-	//     					-> this.getAttribute('tabindex');
-	//
-	    // $(css.nav).removeClass('active'); ->
-// JQUERY
-// $(el).removeClass(className);
-// ->
-// IE8+
-// if (el.classList)
-//   el.classList.remove(className);
-// else
-//   el.className = el.className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
-//
-	//     $(this).addClass('active');
-	//
-	//     if(category === 'navConsumer') {
-	//       $(css.thumbnail).removeClass('selected');
-	//       $(css.consumer).addClass('selected');
-	//     }
-	//     else if(category === 'navMobile') {
-	//       $(css.thumbnail).removeClass('selected');
-	//       $(css.mobile'.mobile').addClass('selected');
-	//     }
-	//     else if(category ==='navCommerce') {
-	//       $(css.thumbnail).removeClass('selected');
-	//       $(css.commerce'.commerce').addClass('selected');
-	//     }
-	//     else if(category ==='navEnterprise') {
-	//       $(css.thumbnail).removeClass('selected');
-	//       $(css.enterprise'.enterprise').addClass('selected');
-	//     }
-	//     else if(category ==='navAll') {
-	//       $(css.thumbnail).removeClass('selected');
-	//     }
-	//   });
-	// };
 	render() {
 		return(
 			<div className='armandoperez'>
