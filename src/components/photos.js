e6088753f0e72fea5855168e44e2f49f7c08e315
trzl47 <brownty47@gmail.com>
2017-04-02 12:05:10 -0500

    initialize object of photographs

---
+++ src/components/photos.js
@@ -0,0 +1,70 @@
+let count = 0;
+const Photos = {
+  'Home':[
+    {
+			title: 'p1',
+      className: 'consumer',
+			img: require('./assets/p1.jpg'),
+			desc: 'looking down at the shoes of a person skateboarding while in motion ',
+			id: count++
+    },
+		{
+			title: 'p2',
+      className: 'enterprise',
+			img: require('./assets/p2.jpg'),
+			desc: 'a organized, full bookshelf in a library and a desk out front',
+			id: count++
+    },
+		{
+			title: 'p3',
+      className: 'consumer',
+			img: require('./assets/p3.jpg'),
+			desc: 'building graffiti of a bearded person and thought bubble of a smartphone',
+			id: count++
+    },
+		{
+			title: 'p4',
+      className: 'commerce',
+			img: require('./assets/p4.jpg'),
+			desc: 'beans on a table',
+			id: count++
+    },
+		{
+			title: 'p5',
+      className: 'enterprise',
+			img: require('./assets/p5.jpg'),
+			desc: 'a hanging paper lantern with east asian characters on it',
+			id: count++
+    },
+		{
+			title: 'p6',
+      className: 'mobile',
+			img: require('./assets/p6.jpg'),
+			desc: 'a hand holding a tablet',
+			id: count++
+    },
+		{
+			title: 'p7',
+      className: 'mobile',
+			img: require('./assets/p7.jpg'),
+			desc: 'an open laptop, headphones, notebook, and pen arranged ona table',
+			id: count++
+    },
+		{
+			title: 'p8',
+      className: 'commerce',
+			img: require('./assets/p8.jpg'),
+			desc: 'coffee cup and box of macarons',
+			id: count++
+    },
+		{
+			title: 'p9',
+      className: 'mobile',
+			img: require('./assets/p9.jpg'),
+			desc: 'over the shoulder perspective of a femme presenting person looking at a smartphone',
+			id: count++
+    },
+  ],
+};
+
+export default Photos;
