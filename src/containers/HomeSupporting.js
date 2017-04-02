90f4940bedfb7e9178c6732a69701c5a7b0929e4
trzl47 <brownty47@gmail.com>
2017-04-02 12:06:48 -0500

    HomeSupporting component
    renders the photographs object array

---
+++ src/containers/HomeSupporting.js
@@ -0,0 +1,54 @@
+// Libs
+import React, { Component } from 'react';
+// Components
+import Photo from '../components/photo.js';
+import photos from '../components/photos.js';
+
+const renderPhotographs = (photos, length) => {
+	const photographs = photos.map((photo) => {
+		return (
+			<div className="col-md-4" key={photo.id}>
+				<Photo
+					title={photo.title}
+					desc={photo.description}
+					rte={photo.description}
+					 />
+			</div>
+		)
+	});
+	const chunkCards = (arr, size) => {
+		let rows = [];
+		for (let i = 0; i < arr.length; i+=size) {
+			rows.push(arr.slice(i,i+size));
+		}
+		return rows;
+	};
+	const renderRows = () => {
+		// let count = 0;
+		return (
+			chunkCards(photographs, 3).map((row, index) => {
+				return (
+					<div className="row" key={index}>
+						{ row }
+					</div>
+				)
+			})
+		)
+	};
+	return (
+		renderRows()
+	);
+};
+
+export default class HomeSupporting extends Component {
+	render() {
+		const photoContent = photos.Home;
+		return (
+			<div className='supporting'>
+				<div className='container'>
+					{ renderPhotographs(photoContent, photoContent.length) }
+				</div>
+			</div>
+		);
+	}
+}
